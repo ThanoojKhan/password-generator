6efb85146510182b04a4715f6b3cea2451155dd5
Thanooj <thanu1495@gmail.com>
2023-11-09 12:22:52 +0530

    second cvommit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ import { generatePassword } from './Utils/passwordGen';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import { faCopy as copyFill } from '@fortawesome/free-solid-svg-icons';
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 
 function App() {
   const [letter, setLetter] = useState(false)
@@ -34,22 +32,21 @@ function App() {
         draggable: true,
         progress: undefined,
         theme: "colored",
-        }); 
+      });
     }
   }
-  const copyPass=()=>{
+  const copyPass = () => {
     setIsCopied(true)
     navigator.clipboard.writeText(Password)
     setTimeout(() => {
       setIsCopied(false);
-    },3000)
+    }, 3000)
   }
   return (
     <div className='mt-32 w-full'>
       <h1 className='font-Press text-3xl text-center'>Password Generator</h1>
       <div className="App w-full flex justify-center">
         <div>
-
           <div className='w-52 mt-10'>
             <div className='flex justify-between my-4'>
               <h1 className='font-Belanosima'>Letters </h1>
@@ -67,22 +64,19 @@ function App() {
               <h1 className='font-Belanosima'>Special Characters </h1>
               <Switch setFn={setSpl} />
             </div>
-
             <div className='flex justify-center'>
               <button onClick={generate} className='bg-black text-white px-2 py-1 rounded border-8 mt-4 border-white  border-dashed'>Generate</button>
             </div>
-
-
           </div>
         </div>
       </div>
-      {Password&&<div className='flex  justify-center mt-12'>
+      {Password && <div className='flex  justify-center mt-12'>
         <div className='p-3 border w-1/3 flex justify-between'>
           <h1 className=' font-Press'>{Password}</h1>
-          <button onClick={copyPass} className=''>{isCopied? <FontAwesomeIcon className='text-lg' icon={copyFill} /> :<FontAwesomeIcon icon={faCopy} />}</button>
+          <button onClick={copyPass} className=''>{isCopied ? <FontAwesomeIcon className='text-lg' icon={copyFill} /> : <FontAwesomeIcon icon={faCopy} />}</button>
         </div>
       </div>}
-      <ToastContainer/>
+      <ToastContainer />
     </div>
   );
 }
